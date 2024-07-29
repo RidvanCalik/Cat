28defdd8606d567d9a7ce31a94f5daddf3f369e2
Rıdvan Çalık <ridvan.calik@portalgrup.com>
2024-07-29 09:44:23 +0300

    update readme and update gitignore

--- CatBackend/CatImages/obj/Debug/net8.0/CatImages.AssemblyInfo.cs
+++ CatBackend/CatImages/obj/Debug/net8.0/CatImages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatImages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b270816f59ac34288cfb53e21e031bcd201cf0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatImages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatImages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
